6408705fc459c64b8d13b341fb1136e800629aaf
eduardobnhr <eduardobnhr@gmail.com>
2023-12-02 00:44:43 -0400

    property binding

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent {
+  pensamento = {
+    id: '1',
+    conteudo: 'aprendendo angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
 
 }
