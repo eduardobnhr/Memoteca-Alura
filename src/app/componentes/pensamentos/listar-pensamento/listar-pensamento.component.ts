c252ce5b5b5e41668f65bf04503d3f82a74dfa77
eduardobnhr <eduardobnhr@gmail.com>
2023-12-03 19:03:45 -0400

    comunicao com o backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,24 +8,6 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
-  listaPensamentos = [
-
-    {
-      conteudo: 'comunicao entre componentes',
-      autoria: 'angular',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'passo informacoes para o componente filho',
-      autoria: 'componente pai',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'para passar informacoes entre componentes, usasse a @Input()',
-      autoria: 'componente ',
-      modelo: 'modelo2'
-    }
-
-  ];
+  listaPensamentos: Pensamento[] = [];
 
 }
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,10 +8,17 @@ import { Component, Input } from '@angular/core';
 })
 export class PensamentoComponent {
 
-    @Input() pensamento = {
-    conteudo: 'amo o angular',
-    autoria: 'luis eduardo',
-    modelo: 'modelo3'
+    @Input() pensamento: Pensamento = {
+      id: 0,
+      conteudo: 'amo o angular',
+      autoria: 'luis eduardo',
+      modelo: 'modelo3'
+  }
+  larguraPensamento(): string{
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
   }
 
 }
