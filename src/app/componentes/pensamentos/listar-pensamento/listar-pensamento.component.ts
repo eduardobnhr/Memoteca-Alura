3c3148706338a71bc354a65586a31467b9bd77b0
eduardobnhr <eduardobnhr@gmail.com>
2023-12-03 11:38:04 -0400

    comunicacao entre componentes

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
-  listaPensamentos = [];
+  listaPensamentos = [
+
+    {
+      conteudo: 'comunicao entre componentes',
+      autoria: 'angular',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'passo informacoes para o componente filho',
+      autoria: 'componente pai',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'para passar informacoes entre componentes, usasse a @Input()',
+      autoria: 'componente ',
+      modelo: 'modelo2'
+    }
+
+  ];
 
 }
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class PensamentoComponent {
 
-  pensamento = {
+    @Input() pensamento = {
     conteudo: 'amo o angular',
     autoria: 'luis eduardo',
     modelo: 'modelo3'
