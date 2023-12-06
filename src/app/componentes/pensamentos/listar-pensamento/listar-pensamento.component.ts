773a64af2745be38e6593071634e6621a6467b1a
eduardobnhr <eduardobnhr@gmail.com>
2023-12-06 13:52:20 -0400

    adicionando o botao de carregar mais

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -11,6 +11,7 @@ export class ListarPensamentoComponent implements OnInit{
 
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
   constructor(private service: PensamentoService) {}
 
@@ -18,6 +19,15 @@ export class ListarPensamentoComponent implements OnInit{
       this.service.listar(this.paginaAtual).subscribe((listaPensamentos) =>{
         this.listaPensamentos = listaPensamentos
       })
+  }
+  carregarMaisPensamentos(){
+   this.service.listar(++this.paginaAtual)
+   .subscribe(listaPensamentos => {
+    this.listaPensamentos.push(...listaPensamentos);
+    if(!listaPensamentos.length){
+      this.haMaisPensamentos = false
+    }
+   })
   }
 
 }
